906cb150497069a21781eb63ed83217b1243dbd9
OksanaMurdza <31917260+OksanaMurdza@users.noreply.github.com>
2018-10-23 20:59:13 +0300

    Update Downloader.java

--- Downloader.java
+++ Downloader.java
@@ -1,12 +1,14 @@
+package Core;
+
 import java.io.*;
 import java.net.*;
 
 
 public class Downloader {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        int a;
-        int n =0;
+    public static void main(String[] args) throws IOException {
+        int number;
+        int partnamedownloadfile =0;
 
         System.out.println("Choose what with cat you want and write number");
         System.out.println("1 - random photo");
@@ -15,36 +17,33 @@ public class Downloader {
         System.out.println("4 - random cat saying hello");
         System.out.println("0 - Esc");
 
-        for (; ; ) {
+        while (true){
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            a = Integer.parseInt(br.readLine());
-            if (a== 0) break;
+            number = Integer.parseInt(br.readLine());
+            if (number == 0) break;
             else {
-                String str2 = Integer.toString(n);
-                n++;
-                Downloader dowloader = new Downloader();
-                switch (a) {
+                String str2 = Integer.toString(partnamedownloadfile);
+                partnamedownloadfile++;
+
+                switch (number) {
                     case 1:
 
-                        dowloader.downloadFiles("https://cataas.com/cat", "C:\\Users\\Admin\\IdeaProjects\\untitled\\src\\test" + str2 + ".jpg", 1);
+                        downloadFiles("https://cataas.com/cat", "./src/test" + str2 + ".jpg", 1);
                         break;
                     case 2:
 
-                        dowloader.downloadFiles("https://cataas.com/cat/cute", "C:\\Users\\Admin\\IdeaProjects\\untitled\\src\\test" + str2 + ".jpg", 1);
+                        downloadFiles("https://cataas.com/cat/cute", "./src/test" + str2 + ".jpg", 1);
 
                         break;
                     case 3:
 
-                        dowloader.downloadFiles("https://cataas.com/cat/gif", "C:\\Users\\Admin\\IdeaProjects\\untitled\\src\\test" + str2 + ".gif", 1);
+                        downloadFiles("https://cataas.com/cat/gif", "./src/test" + str2 + ".gif", 1);
                         break;
                     case 4:
-                        dowloader.downloadFiles("https://cataas.com/cat/says/hello", "C:\\Users\\Admin\\IdeaProjects\\untitled\\src\\test"+ str2 +".jpg", 1);
-                        break;
-                    case 0:
+                        downloadFiles("https://cataas.com/cat/says/hello", "./src/test"+ str2 +".jpg", 1);
                         break;
                     default:
                         System.out.println("Write correct number");
-
 
                 }
 
@@ -54,31 +53,26 @@ public class Downloader {
 
         public static void downloadFiles(String strURL, String strPath, int buffSize)  {
 
-            try {
-
-
+            try  (OutputStream writer = new FileOutputStream(strPath)) {
                 URL connection = new URL(strURL);
-                HttpURLConnection urlconn;
-                urlconn = (HttpURLConnection) connection.openConnection();
+                HttpURLConnection urlconn = (HttpURLConnection) connection.openConnection();
                 urlconn.setRequestMethod("GET");
                 urlconn.connect();
-                InputStream in = null;
-                in = urlconn.getInputStream();
+                InputStream in = urlconn.getInputStream();
 
-                OutputStream writer = new FileOutputStream(strPath);
 
                 byte buffer[] = new byte[buffSize];
                 int c = in.read(buffer);
-                while (c > 0) {
+                 do {
                     writer.write(buffer, 0, c);
                     c = in.read(buffer);
-                }
+                } while (c > 0);
                 writer.flush();
-                writer.close();
                 in.close();
 
             } catch (Exception e) {
                 System.out.println(e);
+                System.out.println("Problem with download file");
             }
         }
     }
